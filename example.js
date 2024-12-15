387219ab0c58ffc3a28c9fa87ee7aa9897f53189
nebilesamelsuleymna <nebilesmaelprog@gmail.com>
2024-12-14 22:38:22 -0900

    worker_thread, for cpu intensive works

--- example.js
+++ example.js
@@ -1,29 +1,24 @@
 const http = require('http');
 const querystring = require('querystring');
 const url = require('url');
+const { Worker } = require('worker_threads');
  
-const server = http.createServer((req, res) => {
-    res.write('Your request is being processed... \n');
-    const query = url.parse(req.url).query;
-    const n = Number(querystring.parse(query)['n']);
-    findSum(n, (sum) => {
-    res.end(`The sum is ${sum} \n`);
+const server = http.createServer(async (req, res) => {
+  res.write('Your request is being processed... \n');
+  const query = url.parse(req.url).query;
+  const n = Number(querystring.parse(query)['n']);
+  const sum = await findSum(n);
+  res.end(`the sum is ${sum}`);
 });
-});
-
+ 
 server.listen(3000, () => {
-console.log('server listening on port 3000');
+  console.log('server listening on port 3000');
 });
-
-function findSum(n, sumCallBack) {
-    let sum = 0;
-    function add(i, cb) {
-        sum = sum + i;
-        if (i == n) {
-        return cb(sum);
-        }
-    setImmediate(add.bind(null, i + 1, cb));
-}
-
-add(1, sumCallBack);
+ 
+function findSum(n) {
+  return new Promise((resolve, reject) => {
+    const worker = new Worker('./worker/summation.js', { workerData: { n } });
+    worker.on('message', resolve);
+    worker.on('error', reject);
+  });
 }
