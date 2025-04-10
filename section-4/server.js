633c3c013f6a317c28d01d45c293bd70ddff6e3d
nebilesamelsuleymna <nebilesmaelprog@gmail.com>
2025-04-10 04:09:18 -0900

    xss-attack,clickjacking and MIME sniffing attack

--- section-4/server.js
+++ section-4/server.js
@@ -1,27 +1,44 @@
-const express = require('express');
-const app = express();
-const ApiError = require('./utils/ApiError');
-const AllRouter = require('./routes/blogroutes');
-const httpStatus=require('http-status')
-const { errorHandler, errorConverter } = require('./midlwares/errors');
-const morgan =require('./config/morgan')
+const express = require('express')
+const app = express()
+const ApiError = require('./utils/ApiError')
+const AllRouter = require('./routes/blogroutes')
+const httpStatus = require('http-status')
+const { errorHandler, errorConverter } = require('./midlwares/errors')
+const morgan = require('./config/morgan')
 const passport = require('passport')
-const {jwtStrategy}= require('./config/passport')
-
+const { jwtStrategy } = require('./config/passport')
+const { xss } = require('express-xss-sanitizer')
+const helmet = require('helmet')
 
 // jwt authentication
-app.use(passport.initialize());
-passport.use('jwt', jwtStrategy);
+app.use(passport.initialize())
+passport.use('jwt', jwtStrategy)
 
-app.use(express.json());
-app.use(AllRouter);
+app.use(express.json())
+
+//security
+app.use(xss())
+app.use(
+	helmet.contentSecurityPolicy({
+		directives: {
+			default: ["'self'"],
+			styleSrc: ["'selft'"],
+			scriptSrc: ["'self'", "'unsafe-inline'"],
+			fontSrc: ["'self'", "'fonts.gstatic.com'"],
+		},
+		reportOnly: true,
+	})
+)
+app.use(helmet.frameguard({ action: 'sameorigin' }))
+app.use(helmet.noSniff())
+app.use(AllRouter)
 
 // path not found 404
 app.use((req, res, next) => {
-    next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
-});
+	next(new ApiError(httpStatus.NOT_FOUND, 'Not found'))
+})
 
-app.use(errorHandler);
-app.use(errorConverter);
+app.use(errorHandler)
+app.use(errorConverter)
 
-module.exports= app;
+module.exports = app
